6dcd3ff7566720d828d8b4c4116f79afc2ff4775
reddeviljoypow1 <115789994+reddeviljoypow1@users.noreply.github.com>
2023-07-24 15:43:24 +0530

    Create generator.py
    Dic🥴 dom

---
+++ generator.py
@@ -0,0 +1,22 @@
+# python
+from insta import accounts
+from instabot import sub, antiban
+from time import sleep
+from os import request
+
+account = "[insta.account/avatar/post]"
+sub = "inputed.user.username"
+
+sub = input("Please insert your username without the @ >")
+
+while True:
+    account.request = 1
+    sub.request = sub
+    sleep(0.3)
+
+# This will generate an account, follow your Instagram and loop it.
+
+while sub < 5000:
+    sleep(24 * 60 * 60)  # Sleep for 24 hours
+    antiban.on()
+    run.loop()
